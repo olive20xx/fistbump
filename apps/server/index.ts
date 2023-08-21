125fafd16f43f45be6925b5deb455a1f2a409c43
mutotu <muttalip.aydemir@gmail.com>
2023-08-21 03:59:43 -0700

    configured graphql

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,7 +1,28 @@
 import express from 'express'
+import { graphqlHTTP } from "express-graphql"
+import { graphql, buildSchema } from 'graphql';
 import mongoose from 'mongoose'
 
+const schema = buildSchema(`
+type Query {
+  hello: String
+}
+`)
+const root = {
+  hello: () => {
+    return "Hello world!"
+  },
+}
+
 const app = express()
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+)
 
 app.listen(8080, () => {
   console.log('360 review server is listening on port 8080! 🤜🏼🤛🏼')
