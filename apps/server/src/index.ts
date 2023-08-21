634d022113e0f37d91ba5617335bae3ae650b729
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-08-21 16:25:34 +0200

    add types

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,9 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import { MONGODB_URL } from './lib/constants'
 import router from './router'
- 
-const cors = require('cors')
-
+import cors from 'cors'
 
 const app = express()
 app.use(express.json())
@@ -14,12 +12,6 @@ app.use(router)
 
 app.listen(8080, () => {
   console.log('connectig to db...', MONGODB_URL)
-  // mongoose
-  //   .connect(MONGODB_URL)
-  //   .then(() => console.log('connected to db from index!'))
-  //   .catch((error) => console.log(error))
-
- 
 
   console.log('360 review server is listening on port 8080! 🤜🏼🤛🏼')
 })
