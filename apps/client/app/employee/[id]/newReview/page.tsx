36917b1cd69036838efa28bb59ae5f76d83e1196
Hrvoje Vincek <vincekhrvoje@hervo.local>
2023-08-22 18:03:45 +0200

    structure the page

--- apps/client/app/employee/[id]/newReview/page.tsx
+++ apps/client/app/employee/[id]/newReview/page.tsx
@@ -1,10 +1,23 @@
 
+import '../../../../app/global.css'
 
 export default function newReview() {
   return (
-    <div>
-      <h2 className='text-3xl font-bold bg-slate-400 text-center'>Welcome to new review page</h2>
-      <p className='text-3xl font-bold bg-slate-400 text-center'>Write a review!</p>
+    <div className="w-full max-h-full bg-slate-500">
+        <div className="bg-slate-500">
+          <div>
+            <h2 className="">Subject of review</h2>
+            <img src="" alt="" />
+            <p>Name</p>
+            <p>Title</p>
+            <p>Team</p>
+          </div>
+        </div>
+        <div className="bg-slate-600">QUESTIONS</div>
+        <div className="bg-slate-700">
+          <h1>PROFILE PICTURE</h1>
+          <img src="" alt="" />
+        </div>
     </div>
   );
 }
