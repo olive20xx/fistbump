fd30cb06765a74649fe21bf1a3c4dda582e60458
Hrvoje Vincek <vincekhrvoje@hervo.local>
2023-08-22 19:39:19 +0200

    structure the page2

--- apps/client/app/employee/[id]/newReview/page.tsx
+++ apps/client/app/employee/[id]/newReview/page.tsx
@@ -14,23 +14,23 @@ export default function newReview() {
   }
 
   return (
-    <div className="flex w-full max-h-full bg-slate-500">
-      <div className="bg-slate-500">
-        <div>
+    <div className=" flex mx-auto justify-between max-w-6xl  h-screen bg-slate-500">
+      <div className="bg-gray-500">
           <h2 className="">Subject of review</h2>
           <img src="" alt="" />
           <p>Name</p>
           <p>Title</p>
           <p>Team</p>
-        </div>
       </div>
-      <div className="bg-slate-600">
+      
+      <div className="bg-gray-700">
         {MOCK_DATA.map(datum => <Metric key={datum.id} question={datum.question} name="name" value="" onChange={handleChange} onClick={handleClick} />)}
       </div>
-      <div className="bg-slate-700">
+
+      <div className="bg-slate-100">
         <h1>PROFILE PICTURE</h1>
         <img src="" alt="" />
-        M</div>
+      </div>
     </div>
   );
 }
