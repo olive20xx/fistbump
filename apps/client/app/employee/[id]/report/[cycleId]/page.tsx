b1f0d848959024ab28746b8bd078f5aad13dee1f
mutotu <muttalip.aydemir@gmail.com>
2023-08-23 10:35:53 -0700

    added some funcs to retrive data

--- apps/client/app/employee/[id]/report/[cycleId]/page.tsx
+++ apps/client/app/employee/[id]/report/[cycleId]/page.tsx
@@ -2,30 +2,40 @@ import axios from 'axios'
 import '../../../../global.css'
 import { getReport, getUser } from '@/lib/fetch'
 
+
+const userQuery = `
+query getUser($id: String) {
+  getUser(id:$id) {
+    fullName
+}
+}`
+
+const reportQuery = `
+query GetReport($targetId: String!, $cycleId: String!) {
+  getReport(targetId: $targetId, cycleId: $cycleId) {
+    _id {
+      target
+      cycle
+    }
+    remarks
+  }
+}`
+
+
 async function Report({ params }) {
   const targetId = params.id
   const cycleId = params.cycleId
-  const query = `
-      query GetReport($targetId: String!, $cycleId: String!) {
-        getReport(targetId: $targetId, cycleId: $cycleId) {
-          _id {
-            target
-            cycle
-          }
-          remarks
-        }
-      }`
 
   // const status = undefined
   const variables = { targetId, cycleId }
 
-  const report = await getReport(query, variables)
-  const user = await getUser(targetId)
+  const report = await getReport(reportQuery, variables)
+  const user = await getUser(targetId, userQuery)
 
   return (
     // (!status ? <p>come back later </p> :
     <div className="p-12">
-      <h1 className="text-2xl">Your Report {user}</h1>
+      <h1 className="text-2xl">Your Report {user.fullName}</h1>
       <div>
         <p>Remarks:</p>
         <p>{report.remarks}</p>
--- apps/client/lib/fetch.ts
+++ apps/client/lib/fetch.ts
@@ -2,22 +2,14 @@ import axios from 'axios'
 
 const url = 'http://localhost:8080/graphql'
 
-export async function getUser(id) {
-  const query = `
-  query getUser($id: String) {
-    getUser(id:$id) {
-      fullName
-  }
-}`
-
+export async function getUser(id, query) {
   const variables = { id }
-
   try {
     const response = await axios.post(url, {
       query,
       variables,
     })
-    return response.data.data.getUser.fullName
+    return response.data.data.getUser
   } catch (error) {
     console.error(error)
   }
