d6cc8ed739ac5ce580c6a3d970d2a74af4f9aa7a
Hrvoje Vincek <vincekhrvoje@hervo.local>
2023-08-23 11:42:32 +0200

    created report slug page2

--- apps/client/app/employee/[id]/report/[id]/page.tsx
+++ apps/client/app/employee/[id]/report/[id]/page.tsx
@@ -1,11 +1,52 @@
+import axios from 'axios'
 
-
-export default function Report() {
-    return (
-      <div>
-        <h2 className='text-3xl font-bold bg-slate-400 text-center'>Welcome to your report page</h2>
-        <p className='text-3xl font-bold bg-slate-400 text-center'>Read your report!</p>
-      </div>
-    );
+async function getReport(query, variables) {
+  try {
+    const response = await axios.post('http://localhost:8080/graphql', {
+      query,
+      variables,
+    })
+    return response.data.data.getReport
+    console.log('response', response.data.data.getReport)
+  } catch (error) {
+    console.error(error)
   }
-  
+}
+
+
+async function Report() {
+
+    const targetId = '64e5c381429c4fdf374cec4c'
+    const cycleId = '64e5c381429c4fdf374cec57'
+    const query = `
+      query GetReport($targetId: String!, $cycleId: String!) {
+        getReport(targetId: $targetId, cycleId: $cycleId) {
+          _id {
+            target
+            cycle
+          }
+          remarks
+          status  
+        }
+      }`
+
+    const variables = { targetId, cycleId }
+
+    const report = await getReport(query, variables)
+
+
+  return <div>
+    <h2>Your Report (student/employee name)</h2>
+    <div>
+      <p>Remarks:</p>
+      
+      <p>
+        {report.remarks}
+      </p>
+      <p>Status:</p>
+      <p>{report.status}</p>
+    </div>
+  </div>
+}
+
+export default Report
