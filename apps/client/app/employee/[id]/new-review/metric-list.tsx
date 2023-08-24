cd1340a35d88c71c603fa3c06c0f53521905bca7
Craig Ostrin <craigostrin@gmail.com>
2023-08-24 17:41:54 +0200

    added submit validation and dashboard link

--- apps/client/app/employee/[id]/new-review/metric-list.tsx
+++ apps/client/app/employee/[id]/new-review/metric-list.tsx
@@ -5,6 +5,7 @@ import Metric from '@/components/ui/metric'
 import { updateReport } from '@/lib/fetch'
 import { GradeData, ReportData } from '@/types/models'
 import { useEffect, useState } from 'react'
+import Link from 'next/link'
 
 const mutation = `
   mutation updateReport($targetId:String!, $cycleId:String!, $input:ReportInput!) {
@@ -28,9 +29,14 @@ const mutation = `
 function SubmittedReview() {
   return (
     <div className="w-1/2 border-2 p-4">
-      <Button disabled className="bg-green-500 disabled:opacity-100">
-        Review submitted!
-      </Button>
+      <div className="mt-10 flex gap-6 justify-center">
+        <Button disabled className="bg-green-500 disabled:opacity-100">
+          Review submitted!
+        </Button>
+        <Link href={'/dashboard'}>
+          <Button>Back to Dashboard</Button>
+        </Link>
+      </div>
     </div>
   )
 }
@@ -76,6 +82,10 @@ export default function MetricList({
   }
 
   const handleSubmit = () => {
+    for (let i = 0; i < state.length; i++) {
+      const gradeData = state[i]
+      if (gradeData.rating === 0 || gradeData.comment === '') return
+    }
     mutationVars.input.reviews.peer[0].grades = state
     mutationVars.input.reviews.peer[0].submitted = true
     updateReport(mutation, mutationVars)
