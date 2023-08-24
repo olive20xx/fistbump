8950a75169d80334a438d702652f5eebb66b09bc
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-08-24 16:07:00 +0200

    feat: login and redirect to dashboard

--- apps/server/src/router.ts
+++ apps/server/src/router.ts
@@ -6,7 +6,7 @@ const router = express.Router()
 
 router.get('/', async (_, res) => {
   try {
-    const data = await Report.find()
+    const data = await User.find()
     console.log('data', data)
     res.json(data)
   } catch (error) {
