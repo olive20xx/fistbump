b443cb13b41ee4caa3a80eecbb5279854f2a9837
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-08-24 16:38:34 +0200

    refactor: use .env file instead of passing url link !!!DO NOT FORGET TO CREATE .ENV FILE ON YOUR MACHINE

--- apps/client/lib/fetch.ts
+++ apps/client/lib/fetch.ts
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
-const url = 'http://localhost:8080/graphql'
+const url = process.env.GRAPHQL_API_URL
 
 export async function getUser(id, query) {
+ 
   const variables = { id }
   try {
     const response = await axios.post(url, {
