e0f72f7407c39ff3b2b0e432cfaee4893f51d267
Craig Ostrin <craigostrin@gmail.com>
2023-08-25 11:37:58 +0200

    styled

--- apps/client/app/login/page.tsx
+++ apps/client/app/login/page.tsx
@@ -1,11 +1,12 @@
-
-import SignInForm from '@/components/form/SignInForm';
+import SignInForm from '@/components/form/SignInForm'
 import '../global.css'
 
 export default function Login() {
   return (
-    <div>
-      <SignInForm></SignInForm>
+    <div className="mt-32 flex flex-col h-screen">
+      <div className="w-1/2 mx-auto p-20 border-slate-200 border-2 rounded-xl shadow-lg">
+        <SignInForm></SignInForm>
+      </div>
     </div>
-  );
+  )
 }
--- apps/client/components/form/SignInForm.tsx
+++ apps/client/components/form/SignInForm.tsx
@@ -1,6 +1,6 @@
-'use client';
+'use client'
 
-import { useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form'
 import {
   Form,
   FormControl,
@@ -8,15 +8,15 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from '../ui/form';
-import * as z from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { Input } from '../ui/input';
-import { Button } from '../ui/button';
-import Link from 'next/link';
-import { getUserByEmail } from '@/lib/fetch';
-import { setCookie } from 'cookies-next';
-import { useRouter } from 'next/navigation';
+} from '../ui/form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Input } from '../ui/input'
+import { Button } from '../ui/button'
+import Link from 'next/link'
+import { getUserByEmail } from '@/lib/fetch'
+import { setCookie } from 'cookies-next'
+import { useRouter } from 'next/navigation'
 
 const FormSchema = z.object({
   email: z.string().min(1, 'Email is required').email('Invalid email'),
@@ -26,12 +26,10 @@ const FormSchema = z.object({
 
     //change later to 8 charachters
     .min(3, 'Password must have than 8 characters'),
-});
+})
 
 const SignInForm = () => {
-
   const { push } = useRouter()
-
 
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
@@ -39,7 +37,7 @@ const SignInForm = () => {
       email: '',
       password: '',
     },
-  });
+  })
 
   async function onSubmit(values: z.infer<typeof FormSchema>) {
     const query = `
@@ -49,33 +47,30 @@ const SignInForm = () => {
     }
     }`
 
-
-
     const email = values.email
     const password = values.password
     const variables = { email, password }
 
-
-    const userFound = await getUserByEmail(query, variables);
+    const userFound = await getUserByEmail(query, variables)
     if (userFound.fullName) {
-      console.warn('WELCOME', userFound.fullName);
+      console.warn('WELCOME', userFound.fullName)
       setCookie('user', userFound.fullName)
       push('/dashboard')
     }
-  };
+  }
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className='w-full'>
-        <div className='space-y-2'>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
+        <div className="space-y-2">
           <FormField
             control={form.control}
-            name='email'
+            name="email"
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Email</FormLabel>
                 <FormControl>
-                  <Input placeholder='mail@example.com' {...field} />
+                  <Input placeholder="mail@example.com" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -83,14 +78,14 @@ const SignInForm = () => {
           />
           <FormField
             control={form.control}
-            name='password'
+            name="password"
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Password</FormLabel>
                 <FormControl>
                   <Input
-                    type='password'
-                    placeholder='Enter your password'
+                    type="password"
+                    placeholder="Enter your password"
                     {...field}
                   />
                 </FormControl>
@@ -99,21 +94,21 @@ const SignInForm = () => {
             )}
           />
         </div>
-        <Button className='w-full mt-6' type='submit'>
+        <Button className="mx-auto block w-1/3 mt-6" type="submit">
           Sign in
         </Button>
       </form>
-      <div className='mx-auto my-4 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400'>
+      <div className="mx-auto my-4 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
         or
       </div>
-      <p className='text-center text-sm text-gray-600 mt-2'>
+      <p className="text-center text-sm text-gray-600 mt-2">
         If you don&apos;t have an account, please&nbsp;
-        <Link className='text-blue-500 hover:underline' href='/sign-up'>
+        <Link className="text-blue-500 hover:underline" href="/sign-up">
           Sign up
         </Link>
       </p>
     </Form>
-  );
-};
+  )
+}
 
-export default SignInForm;
+export default SignInForm
