017446c3ba5b5f5c998102bf511afafdf869ff0f
mutotu <muttalip.aydemir@gmail.com>
2023-08-29 07:18:32 -0700

    added tests for Header2 and signInForm comps

--- apps/client/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  moduleNameMapper: {
-    '\\.(css|scss)$': 'identity-obj-proxy', // Mock CSS modules
-  },
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-};
---
+++ apps/client/tests/header2.test.tsx
@@ -0,0 +1,20 @@
+import '@testing-library/jest-dom'
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Header2 } from '../components/typography/header2'; // Update the path based on your actual component location
+
+describe('Header2', () => {
+  it('renders the header with children', () => {
+    const { getByText } = render(<Header2>Test Header</Header2>);
+    const headerElement = getByText('Test Header');
+    expect(headerElement).toBeInTheDocument();
+    expect(headerElement.tagName).toBe('H2');
+    expect(headerElement).toHaveClass('scroll-m-20');
+    expect(headerElement).toHaveClass('pb-2');
+    expect(headerElement).toHaveClass('text-2xl');
+    expect(headerElement).toHaveClass('font-semibold');
+    expect(headerElement).toHaveClass('tracking-tight');
+    expect(headerElement).toHaveClass('transition-colors');
+    expect(headerElement).toHaveClass('first:mt-0');
+  });
+});
