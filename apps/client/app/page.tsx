d807b2df47468d7476df0d70246f3d708bcf778d
mutotu <muttalip.aydemir@gmail.com>
2023-08-30 02:42:33 -0700

    fixed the issue to go login page when there is no user stored in cookies

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,19 +1,20 @@
-
+import { cookies } from 'next/headers'
 import { Button } from '@/components/ui/button';
 import '@/app/global.css'
 import Link from 'next/link';
 
 export default function Page() {
+  const cookieStore = cookies()
+  const loggedUserFullName = cookieStore.get('user').value
   return (
     <div className='flex flex-col items-center h-screen gap-10 justify-center'>
       <h2 className='text-3xl font-bold bg-pink-400 text-center'>WELCOME TO 360 REVIEW </h2>
-      <Link href={'/dashboard'}>
+      <Link href={loggedUserFullName ? '/dashboard' : '/login'}>
         <Button>Go to dashboard</Button>
       </Link>
       <Link href={'/managerpanel'}>
         <Button>Go to managerpanel</Button>
       </Link>
-
     </div>
   );
 }
