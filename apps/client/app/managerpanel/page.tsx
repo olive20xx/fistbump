ec4c1495650932cfb93a22f1419235f063330a0d
Hrvoje Vincek <vincekhrvoje@hervo.local>
2023-09-01 00:33:11 +0200

    displayed cycle details on

--- apps/client/app/managerpanel/page.tsx
+++ apps/client/app/managerpanel/page.tsx
@@ -7,7 +7,7 @@ import { UserModel } from '../../../../packages/types/models'
 import { getAllUsers, getCurrentCycle } from '@/lib/get-data-api'
 
 
-function UserItem({ user, cycleId }) {
+function UserItem({ user, cycle }) {
   return (
     <div className="grid grid-cols-5 gap-4 border-b p-2 bg-white items-center">
       <p className="font-semibold">{user.title}</p>
@@ -16,7 +16,7 @@ function UserItem({ user, cycleId }) {
       <Link href={`/employee/${user._id}/new-review`}>
         <Button>Write a report</Button>
       </Link>
-      <Link href={`/employee/${user._id}/manager-report/${cycleId}`}>
+      <Link href={`/employee/${user._id}/manager-report/${cycle.cycleId}`}>
         <Button>View Full Report</Button>
       </Link>
 
@@ -26,8 +26,6 @@ function UserItem({ user, cycleId }) {
 
 export default async function ManagerPanel() {
   const cycle = await getCurrentCycle()
-  const cycleId = cycle._id
-
   const users = await getAllUsers()
 
   return (
@@ -37,13 +35,14 @@ export default async function ManagerPanel() {
         <h2 className="text-3xl font-bold">List of the users</h2>
       </div>
       <div className="rounded-xl max-w-7xl mx-auto">
+        <div className="flex gap-10">{cycle.title.split(',')[0]} <p>End date: {cycle.endDate.split('T')[0]}</p></div>
         <div className="grid grid-cols-10 gap-4 font-bold border-b p-2 bg-slate-400">
           <p className="col-span-2">Title</p>
           <p className="col-span-2">Full Name</p>
           <p className="col-span-2">Team Name</p>
         </div>
         {users.map((user: UserModel) => (
-          <UserItem key={user.fullName} user={user} cycleId={cycleId} />
+          <UserItem key={user.fullName} user={user} cycle={cycle} />
         ))}
       </div>
     </div>
