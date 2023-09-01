6cc5620c658bb0d02bd88ca3c776d3935a925dd2
Craig Ostrin <craigostrin@gmail.com>
2023-09-01 12:47:24 +0200

    deleted stray util file

--- packages/util.ts
+++
@@ -1,6 +0,0 @@
-import { ReviewData } from '../apps/client/types/models'
-import { ReviewModel } from './types/models'
-
-type ReviewType = ReviewData | ReviewModel
-
-export function findReviewByReviewerId<ReviewType>(review: ReviewType) {}
