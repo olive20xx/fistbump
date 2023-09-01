da42382f6365023295dabbbcc5328477c0f7a109
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-09-01 19:46:29 +0200

    refactor: 1 combobox insted of 3

--- apps/client/components/Combobox.tsx
+++ apps/client/components/Combobox.tsx
@@ -22,30 +22,29 @@ import { useLazyQuery, useMutation, useQuery } from "@apollo/client"
 export const revalidate = 0
 export const fetchCache = 'force-no-cache'
 export default function NominationBox({ users, loggedUserId, cycleId }) {
-  const [open, setOpen] = React.useState("")
+  const [open, setOpen] = React.useState(false)
   const [currentValue, setCurrentValue] = React.useState('')
-
-  const [popoverStates, setPopoverStates] = React.useState([])
+  const [value, setValue] = React.useState('')
   const [peerId, setPeerId] = React.useState(null)
   const [peers, setPeers] = React.useState(null)
   const [updatePeerReviews] = useMutation(mutations.UPDATE_PEER_REVIEWS)
-  const [getNominations] = useLazyQuery(queries.GET_PEER_REVIEWS)
-  console.log(popoverStates)
+  const [getNominationData] = useLazyQuery(queries.GET_PEER_REVIEWS)
+
 
   React.useEffect(() => {
     async function nominations() {
-      const { data: { getReport: { reviews: { peers } } } } = await getNominations({
+      const { data: { getReport: { reviews: { peers } } } } = await getNominationData({
         variables: { targetId: loggedUserId, cycleId }
       })
+      const filteredPeers = peers.filter((peer) => peer.reviewerId === null)
+      setPeers(filteredPeers)
       console.log('nominations===>', peers)
-      setPeers(peers)
-      setPopoverStates(peers.map(() => ({ value: '', disabled: false })));
     }
     nominations()
-  }, [])
+  }, [cycleId, getNominationData, loggedUserId])
 
 
-  async function handleNominatePeer(index) {
+  async function handleNominatePeer() {
     const mutationVars = {
       targetId: loggedUserId,
       cycleId: cycleId,
@@ -53,61 +52,58 @@ export default function NominationBox({ users, loggedUserId, cycleId }) {
         { newReviewerId: peerId }
     }
     const { data: { updatePeerReviewerId: { reviews: { peers } } } } = await updatePeerReviews({ variables: mutationVars })
-    console.log('reviewerId is changing===>', peers)
-    const updatedPopoverStates = [...popoverStates]
-    updatedPopoverStates[index] = { ...updatedPopoverStates[index], disabled: true }
-    setPopoverStates(updatedPopoverStates)
+    const filteredPeers = peers.filter((peer) => peer.reviewerId === null)
+    setPeers(filteredPeers)
   }
 
   return (
     (!peers ? <p>Loading</p> :
-      (peers?.map((peer, index) => (
-        <div className="pt-12" key={peer._id}>
-          <Popover open={open === peer._id} onOpenChange={() => setOpen(peer._id)} >
-            <PopoverTrigger asChild>
-              <Button
-                disabled={peer.reviewerId !== null || popoverStates[index].disabled}
-                variant="outline"
-                role="combobox"
-                aria-expanded={open === peer._id}
-                className="w-[200px] justify-between"
-              >
-                {popoverStates[index].value
-                  ? popoverStates[index].value
-                  : "Select a peer"}
-                <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-[200px] p-0">
-              <Command>
-                <CommandInput placeholder="Find a peer" className="h-9" />
-                <CommandEmpty>No peer found</CommandEmpty>
-                <CommandGroup>
-                  {users.map((user) => (
-                    <CommandItem
-                      key={user._id}
-                      onSelect={(value) => {
-                        setPeerId(user._id)
-                        const updatedStates = [...popoverStates]
-                        updatedStates[index].value = value === user.fullName ? "" : value
-                        setOpen('')
-                      }}
-                    >
-                      {user.fullName}
-                      <CheckIcon
-                        className={cn(
-                          "ml-auto h-4 w-4",
-                          currentValue === user.fullName ? "opacity-100" : "opacity-0"
-                        )}
-                      />
-                    </CommandItem>
-                  ))}
-                </CommandGroup>
-              </Command>
-            </PopoverContent>
-            <Button disabled={peer.reviewerId !== null || popoverStates[index].disabled} onClick={() => handleNominatePeer(index)}>Nominate</Button>
-          </Popover>
-        </div>
-      )))
+
+      <div className="pt-12"  >
+        <Popover open={open} onOpenChange={() => setOpen(!open)} >
+          <PopoverTrigger asChild>
+            <Button
+              disabled={peers.length === 0}
+              variant="outline"
+              role="combobox"
+              aria-expanded={open}
+              className="w-[200px] justify-between"
+            >
+              {value
+                ? value
+                : "Select a peer"}
+              <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-[200px] p-0">
+            <Command>
+              <CommandInput placeholder="Find a peer" className="h-9" />
+              <CommandEmpty>No peer found</CommandEmpty>
+              <CommandGroup>
+                {users.map((user) => (
+                  <CommandItem
+                    key={user._id}
+                    onSelect={(value) => {
+                      setPeerId(user._id)
+                      setValue(value === user.fullName ? "" : value)
+                      setOpen(false)
+                    }}
+                  >
+                    {user.fullName}
+                    <CheckIcon
+                      className={cn(
+                        "ml-auto h-4 w-4",
+                        currentValue === user.fullName ? "opacity-100" : "opacity-0"
+                      )}
+                    />
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </Command>
+          </PopoverContent>
+          <Button disabled={peers.length === 0} onClick={handleNominatePeer}>Nominate</Button>
+        </Popover>
+      </div>
+
     ))
 }
