aa2e1cbc62bd622b33d176dd8a45a50ab5227815
Hrvoje Vincek <vincekhrvoje@hervo.local>
2023-09-04 15:50:38 +0200

    fixed logout route

--- apps/client/components/Logout.tsx
+++ apps/client/components/Logout.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import { setCookie } from "cookies-next"
+import { setCookie } from 'cookies-next'
 
 export default function handleLogout() {
   setCookie('token', '')
   setCookie('userId', '')
-  window.location.href = '/dashboard'
+  window.location.href = '/'
 }
