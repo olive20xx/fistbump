8041f91a96f41d6d46a7e8deef9c6109a889c892
Hrvoje Vincek <vincekhrvoje@hervo.local>
2023-09-04 14:08:07 +0200

    log in form added to landing-page

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,19 +1,25 @@
+'use client'
 
-import { Button } from '@/components/ui/button';
+import { Button } from '@/components/ui/button'
 import '@/app/global.css'
-import Link from 'next/link';
+import Link from 'next/link'
+import SignInForm from '@/components/form/SingInForm'
 
 export default function Page() {
   return (
-    <div className='flex flex-col items-center h-screen gap-10 justify-center'>
-      <h2 className='text-3xl font-bold bg-pink-400 text-center'>WELCOME TO 360 REVIEW </h2>
+    <div className="flex flex-col items-center h-screen gap-10 justify-center">
+      <h2 className="text-3xl font-bold bg-pink-400 text-center">
+        WELCOME TO 360 REVIEW{' '}
+      </h2>
       <Link href={'/dashboard'}>
         <Button>Go to dashboard</Button>
       </Link>
       <Link href={'/managerpanel'}>
         <Button>Go to managerpanel</Button>
       </Link>
-
+      <div className="w-[550px] mx-auto p-20 border-slate-200 border-2 rounded-xl shadow-lg">
+        <SignInForm></SignInForm>
+      </div>
     </div>
-  );
+  )
 }
