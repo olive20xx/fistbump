7740673d4d1d1a203a399e2f9c64d7b8e9671a55
Craig Ostrin <craigostrin@gmail.com>
2023-09-04 14:03:16 +0200

    added 'href' as optional prop for <Button>

--- apps/client/components/ui/button.tsx
+++ apps/client/components/ui/button.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
+import Link from 'next/link'
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
-
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
@@ -46,11 +46,25 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
   VariantProps<typeof buttonVariants> {
   asChild?: boolean
+  href?: string
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, href = '', asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
+
+    if (href !== '') {
+      return (
+        <Link href={href}>
+          <Comp
+            className={cn(buttonVariants({ variant, size, className }))}
+            ref={ref}
+            {...props}
+          />
+        </Link>
+      )
+    }
+
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
