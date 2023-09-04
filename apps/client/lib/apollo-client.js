4fe170efe9ef873090d6803d43b9d8a6116b5360
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-09-04 16:32:24 +0200

    feat(client): add auth headers for graphql requests

--- apps/client/lib/apollo-client.js
+++ apps/client/lib/apollo-client.js
@@ -1,25 +1,42 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
 import {
   NextSSRInMemoryCache,
   NextSSRApolloClient,
 } from '@apollo/experimental-nextjs-app-support/ssr'
 import { registerApolloClient } from '@apollo/experimental-nextjs-app-support/rsc'
 import { removeTypenameFromVariables } from '@apollo/client/link/remove-typename'
-
+import { cookies } from 'next/headers'
 const options = {
   addTypename: false,
 }
+
+const httpLink = new HttpLink({
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_API_URL,
+  fetchOptions: { cache: 'no-store' },
+})
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  let token
+
+  if (typeof window !== undefined) {
+    const cookieStore = cookies()
+    token = cookieStore.get('token').value
+  }
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  }
+})
 const { getClient } = registerApolloClient(() => {
   const removeTypenameLink = removeTypenameFromVariables()
   return new NextSSRApolloClient({
     cache: new NextSSRInMemoryCache(options),
-    link: new HttpLink({
-      // https://studio.apollographql.com/public/spacex-l4uc6p/
-      uri: process.env.NEXT_PUBLIC_GRAPHQL_API_URL,
-      // you can disable result caching here if you want to
-      // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
-      fetchOptions: { cache: 'no-store' },
-    }),
+    link: authLink.concat(httpLink),
   })
 })
 
