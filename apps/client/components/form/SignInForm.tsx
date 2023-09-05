c5bac27f7d339cab1c0e15e9d8e19a67fd1bf7a0
mutotu <muttalip.aydemir@gmail.com>
2023-09-05 04:22:56 -0700

    added error handler for the error coming from server wheren credentials are inccorect

--- apps/client/components/form/SignInForm.tsx
+++ apps/client/components/form/SignInForm.tsx
@@ -19,6 +19,8 @@ import { setCookie } from 'cookies-next'
 import { useRouter } from 'next/navigation'
 import { queries } from '@/lib/graphql-queries'
 import { useLazyQuery } from '@apollo/client'
+import ErrorHandler from '../ErrorHandler'
+import { useState } from 'react'
 
 const FormSchema = z.object({
   email: z.string().min(1, 'Email is required').email('Invalid email'),
@@ -31,7 +33,7 @@ const FormSchema = z.object({
 
 const SignInForm = () => {
   const [loginUser, { error }] = useLazyQuery(queries.LOGIN);
- 
+  const [errorMessage, setErrorMessage] = useState(null)
 
   const { push } = useRouter()
 
@@ -47,14 +49,12 @@ const SignInForm = () => {
     const email = values.email
     const password = values.password
     const variables = { email, password }
-
-    if (error) return `Error! ${error}`
     const data = await loginUser({ variables })
     if (!data.data) {
       console.log('Invalid credentials')
+      setErrorMessage({ message: 'Invalid credential', code: 'Not Found' })
       return
     }
-
     const login = data.data.login
     if (login.token && login.id) {
       setCookie('token', login.token)
@@ -97,6 +97,9 @@ const SignInForm = () => {
           Log in
         </Button>
       </form>
+      {errorMessage &&
+        <div className="absolute top-0 right-100 mt-20 mr-4 p-2 bg-red-500 text-white rounded-lg shadow-md z-10"><ErrorHandler error={errorMessage} onClose={() => setErrorMessage(null)} /></div>
+      }
       <p className="text-center text-white text-sm text-gray-600 mt-2">
         If you dont have an account, please&nbsp;
         <Link className="text-emerald-200 hover:underline" href="/signup">
