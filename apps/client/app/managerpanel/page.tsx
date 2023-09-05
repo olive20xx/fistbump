990df9c199d98c3a52a80295b54576feaf18cd07
Hrvoje Vincek <vincekhrvoje@hervo.local>
2023-09-05 14:15:06 +0200

    refractor managerpanel. UserItem is a component

--- apps/client/app/managerpanel/page.tsx
+++ apps/client/app/managerpanel/page.tsx
@@ -1,29 +1,7 @@
 import '@/app/global.css'
-import Link from 'next/link'
-import { Button } from '@/components/ui/button'
-import { apolloClient } from '@/lib/apollo-client'
-import { queries } from '@/lib/graphql-queries'
+import { UserItem } from '@/components/UserItem'
+import { modelTypes } from '@/fistbump-types/'
 import { getAllUsers, getCurrentCycle } from '@/lib/get-data-api'
-import { modelTypes } from '@/fistbump-types'
-
-export const dynamic = 'force-dynamic'
-
-function UserItem({ user, cycleId }) {
-  return (
-    <div className="grid grid-cols-8 gap-4 border-b p-2 bg-white items-center">
-      <p className="font-semibold col-span-2">{user.title}</p>
-      <p className='col-span-2'>{user.fullName}</p>
-      <p className='col-span-2'>{user.teamName}</p>
-      <Link href={`/employee/${user._id}/manager-report/${cycleId}`}>
-        <Button>View Full Report</Button>
-      </Link>
-      <Link href={`/employee/${user._id}/new-report`}>
-        <Button>Write Report</Button>
-      </Link>
-    </div >
-  )
-}
-
 export default async function ManagerPanel() {
   const cycle = await getCurrentCycle()
   const cycleId = cycle._id
