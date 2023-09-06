c1d57f6b81876b2defeca7f57eb0b19a472cdb9c
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-09-06 12:40:09 +0200

    test page

---
+++ apps/client/app/testpage/page.tsx
@@ -0,0 +1,102 @@
+import '@/app/global.css'
+import { cookies } from 'next/headers'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+import handleLogout from '@/components/Logout'
+import NominationBox from '@/components/Combobox'
+import {
+  getAllUsers,
+  getCurrentCycle,
+  getAssignedReviews,
+  getUserById,
+  getFullReport,
+} from '@/lib/get-data-api'
+import { redirect } from 'next/navigation'
+import SelfReview from '@/components/review/Self'
+import { User } from '@/src/__generated__/graphql'
+
+import Targets from '@/components/table/Targets'
+
+export const revalidate = 0
+
+export default async function Dashboard() {
+  const cookieStore = cookies()
+
+  const cycle = await getCurrentCycle()
+  const cycleId = cycle._id
+
+  const token = cookieStore.get('token')
+  const id = cookieStore.get('userId')
+
+  if (token === undefined || !token.value || id === undefined || !id.value) {
+    redirect('/')
+  }
+
+  const loggedUser = await getUserById(id.value)
+
+
+  let loggedUserFullName = loggedUser.fullName
+  let isLogged = true
+  let loggedUserId = id.value
+  const loggedUserFirstName = loggedUserFullName
+    ? loggedUserFullName.split(' ')[0]
+    : ''
+
+
+  const loggedUserLastName = loggedUserFullName
+    ? loggedUserFullName.split(' ')[1]
+    : ''
+
+  const assignedReviews = await getAssignedReviews(loggedUserId, cycleId)
+  const assignedUsers = await Promise.all(
+    assignedReviews.map(async (review) => await getUserById(review._id.targetId))
+  )
+
+
+  const reportVars = {
+    targetId: id.value,
+  }
+  const loggedUserReport = await getFullReport(reportVars.targetId)
+  const peers = await getAllUsers()
+
+  return (
+    <div className="bg-slate-200 h-screen">
+      <div className="bg-pink-400 flex px-12 justify-between items-center h-24 text-center mx-auto max-w-7xl">
+        <h2 className="text-3xl font-bold">List of the users</h2>
+        <div>
+          {isLogged && loggedUserFullName ? (
+            <div>
+              <h2>Hello {loggedUserFirstName}</h2>
+              <Button onClick={handleLogout}> Log out</Button>
+            </div>
+          ) : (
+            <Link href={'/login'}>
+              <Button>Log in</Button>
+            </Link>
+          )}
+        </div>
+      </div>
+      <div className="rounded-xl max-w-7xl mx-auto">
+        <div className="grid grid-cols-8 gap-4 font-bold border-b p-2 bg-slate-400">
+          <p className="col-span-2">Title</p>
+          <p className="col-span-2">Full Name</p>
+          <p className="col-span-2">Team Name</p>
+        </div>
+        {assignedUsers.map((user: User) => (
+          <Targets
+            key={user._id}
+            user={user}
+          />
+        ))}
+        <SelfReview user={loggedUser}></SelfReview>
+        <NominationBox
+          users={peers}
+          loggedUserId={loggedUserId}
+          report={loggedUserReport}
+          cycleId={cycleId}
+        ></NominationBox>
+
+      </div>
+    </div>
+  )
+}
