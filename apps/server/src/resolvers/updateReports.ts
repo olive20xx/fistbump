b306b423c50e4ed22423838d7bc04b0494bd7dc6
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-09-06 14:35:28 +0200

    refactor(server):update report -> update self review

--- apps/server/src/resolvers/updateReports.ts
+++ apps/server/src/resolvers/updateReports.ts
@@ -1,8 +1,4 @@
-import {
-  PeerUpdateInput,
-  ReportInput,
-  ReviewInput,
-} from '@/__generated__/resolvers-types'
+import { ReportInput } from '@/__generated__/resolvers-types'
 import { modelTypes } from '@/fistbump-types'
 import Cycle from '../lib/mongoose/models/Cycle'
 import Report from '..//lib/mongoose/models/Report'
@@ -17,7 +13,7 @@ export async function resolveUpdateReport(
   }: {
     targetId: String
     cycleId: String
-    input: ReportInput & PeerUpdateInput & ReviewInput
+    input: ReportInput
   },
   context: ApolloContext
 ) {
@@ -47,40 +43,58 @@ export async function resolveUpdateReport(
   // //if myself -> i can mutate self review or nominate peers!
   if (isSelf) {
     if (report.status === modelTypes.REPORT_STATUS.NOMINATION) {
-      const peersToNominate = report.reviews.peers.find(
-        (peer) => peer.reviewerId === null
-      )
-
-      if (peersToNominate) {
-        peersToNominate.reviewerId = new modelTypes.ObjectId(
-          input.newReviewerId
+      if (input.newReviewerId) {
+        const peersToNominate = report.reviews.peers.find(
+          (peer) => peer.reviewerId === null
         )
-      }
-      await report.save()
-
-      const peerIds = report.reviews.peers.map((peer) => {
-        return {
-          reviewerId: peer.reviewerId,
+        if (peersToNominate) {
+          if (input.newReviewerId)
+            peersToNominate.reviewerId = new modelTypes.ObjectId(
+              input.newReviewerId
+            )
         }
-      })
-      return {
-        reviews: {
-          peers: peerIds,
-        },
+        await report.save()
+
+        const peerIds = report.reviews.peers.map((peer) => {
+          return {
+            reviewerId: peer.reviewerId,
+          }
+        })
+
+        return {
+          reviews: {
+            peers: peerIds,
+          },
+        }
+      } else {
+        //WRITE SELF REVIEW
+        //this should be changed to check if report.status === review
+        if (input.reviews?.self?.grades) {
+          report.reviews.self.grades = input.reviews?.self
+            ?.grades as modelTypes.GradeModel[]
+        }
+        console.log({
+          grades: input.reviews?.self?.grades,
+          submitted: input.reviews?.self?.submitted,
+        })
+        if (
+          input.reviews?.self?.submitted !== undefined &&
+          input.reviews?.self?.submitted !== null
+        ) {
+          report.reviews.self.submitted = input.reviews?.self?.submitted
+        }
+        await report.save()
+        console.log(report.reviews.self)
+        return {
+          reviews: {
+            self: report.reviews.self,
+          },
+        }
       }
     }
-    //WRITE SELF REVIEW
-    //this should be changed to check if report.status === review
-    // if (input.grades) {
-    //   report.reviews.self.grades = input.grades as modelTypes.GradeModel[]
-    // }
-    // if (input.submitted !== undefined && input.submitted !== null) {
-    //   report.reviews.self.submitted = input.submitted
-    // }
-    // await report.save()
-    // return report.reviews.self
   }
 }
+
 //if reviewer->  review the peer
 
 //   const filter = { '_id.targetId': targetId, '_id.cycleId': cycleId }
