fac4e7cfcf6265873ae598f8415a30f160710dcc
Craig Ostrin <craigostrin@gmail.com>
2023-09-07 16:57:13 +0200

    UNPC takes a User prop

--- apps/client/components/ui/UserNamePhotoCaption.tsx
+++ apps/client/components/ui/UserNamePhotoCaption.tsx
@@ -1,12 +1,18 @@
+import { User } from '@/src/__generated__/graphql'
 import Photo from './photo'
+import { getUserById } from '@/lib/get-data-api'
 
 type UNPCProps = {
-  photo: string,
-  fullName: string,
+  userId: string,
   caption: string
 }
 
-function UserNamePhotoCaption({ photo, fullName, caption }: UNPCProps) {
+async function UserNamePhotoCaption({ userId, caption }: UNPCProps) {
+  if (!userId) return
+
+  const user = await getUserById(userId)
+  const { fullName, photo } = user
+
   return (
     <div className='flex'>
       <Photo photo={photo} alt={`Picture of ${fullName}`} />
