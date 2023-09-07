1726c302b3eb7f4441978c1a0e0595bd8f0ecd55
Muttalip Aydemir <36741298+Mutotu@users.noreply.github.com>
2023-09-07 10:29:36 +0000

    refatctored wave comp

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,28 +1,17 @@
 'use client'
-import Wave from 'react-wavify'
 import '@/app/global.css'
 import SignInForm from '@/components/form/SignInForm'
+import WaveImage from '@/components/WaveImage'
 
 export default function Page() {
   return (
-    <div className="border-neutral-50 justify-center bg-backgroundlogin h-screen flex flex-col items-center">
-      <div className="border-neutral-50 flex flex-col items-center w-[299px]">
+    <div className="border-neutral-50 justify-center bg-backgroundlogin h-screen flex flex-col items-center relative">
+      <WaveImage />
+      <div className="border-neutral-50 flex flex-col items-center w-[299px] relative z-10">
         <h1 className="text-5xl text-white gap-4 mb-7">Log In</h1>
         <p className="text-darkturqouise mb-10">Welcome back we missed you!</p>
         <SignInForm />
       </div>
-      <Wave
-        className="opacity-30 absolute bottom-0"
-        fill="#03BCA7"
-        paused={false}
-        style={{ display: 'flex' }}
-        options={{
-          height: 20,
-          amplitude: 20,
-          speed: 0.25,
-          points: 3,
-        }}
-      />
     </div>
   )
 }
