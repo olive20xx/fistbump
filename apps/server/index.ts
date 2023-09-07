8fd83d974363a41c061793f4f619a65f0d2f6875
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-09-07 15:06:13 +0200

    feat: add cors to the server

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,19 +1,20 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import http from 'http'
+import bodyParser from 'body-parser'
 import resolvers from './src/resolvers'
 import { readFileSync } from 'fs'
 import { MONGODB_URL } from './src/lib/constants'
 import { ApolloServer } from '@apollo/server'
-import { startStandaloneServer } from '@apollo/server/standalone'
 import jwtMiddleware from './src/lib/middlewares/jwt'
 import { ApolloContext } from './src/types'
-
+import { expressMiddleware } from '@apollo/server/express4'
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
 const typeDefs = readFileSync('./src/schema.graphql', 'utf-8')
 
 const app = express()
-app.use(express.json())
-app.use(cors())
+const httpServer = http.createServer(app)
 
 mongoose.connect(MONGODB_URL)
 const db = mongoose.connection
@@ -21,18 +22,31 @@ const db = mongoose.connection
 const apolloServer = new ApolloServer<ApolloContext>({
   typeDefs,
   resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 })
+
+async function startServer() {
+  await apolloServer.start()
+}
+
+startServer()
+  .then(() => {
+    app.use(
+      '/',
+      cors<cors.CorsRequest>({
+        origin: ['http://localhost:3000'],
+      }),
+      bodyParser.json(),
+      expressMiddleware(apolloServer, { context: jwtMiddleware })
+    )
+  })
+  .then(() => {
+    new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve))
+  })
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 db.once('open', () => {
   console.log('Connected to MongoDB!')
-})
-
-startStandaloneServer(apolloServer, {
-  listen: { port: 4000 },
-  context: jwtMiddleware,
-}).then(({ url }) => {
-  console.log('🚀 Server ready at', url)
 })
 
 process.on('SIGINT', async () => {
