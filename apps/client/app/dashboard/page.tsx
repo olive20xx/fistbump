a5808cc4b2ee41a7b8a350bbe385367bccdf0767
Craig Ostrin <craigostrin@gmail.com>
2023-09-08 17:05:16 +0200

    centered

--- apps/client/app/dashboard/page.tsx
+++ apps/client/app/dashboard/page.tsx
@@ -51,7 +51,7 @@ export default async function Dashboard() {
   const peers = await getAllUsers()
 
   return (
-    <div className='w-[1220px]'>
+    <div className='w-[1220px] mx-auto'>
       <DashboardTop
         firstName={loggedUserFirstName}
         lastName={loggedUserLastName}
