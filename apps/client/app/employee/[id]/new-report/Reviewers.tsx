0f7761c30aee4bfb41f79552294c21b2dbf2f9ce
Craig Ostrin <craigostrin@gmail.com>
2023-09-08 19:50:46 +0200

    reviewers -> flex-wrap

--- apps/client/app/employee/[id]/new-report/Reviewers.tsx
+++ apps/client/app/employee/[id]/new-report/Reviewers.tsx
@@ -5,10 +5,10 @@ async function Reviewers({ reviewerIds }: { reviewerIds: string[] }) {
   return (
     <div className='p-4'>
       <Header2>Reviewers</Header2>
-      <div className='gap-2 flex-col flex'>
+      <div className='gap-2 flex-wrap flex'>
         {reviewerIds.map(async (id, i) => (
           <UserNamePhotoCaption
-            className='bg-white p-2 w-[250px] rounded-md border-2 border-gray-200 shadow-sm'
+            className='bg-white p-2 min-w-[200px] rounded-md border-2 border-gray-200 shadow-sm'
             userId={id}
             caption={''}
             key={i} />
