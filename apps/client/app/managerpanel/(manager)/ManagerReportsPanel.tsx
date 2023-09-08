25d161fc41a1f7d7d66cdde05ac4312ddfb0b47f
Hrvoje Vincek <vincekhrvoje@hervo.local>
2023-09-08 18:58:46 +0200

    managerpanel rendering photo name title and button

--- apps/client/app/managerpanel/(manager)/ManagerReportsPanel.tsx
+++ apps/client/app/managerpanel/(manager)/ManagerReportsPanel.tsx
@@ -6,6 +6,7 @@ import {
   PanelTitle,
 } from '../../../components/ui/Panel'
 import { getUserById } from '@/lib/get-data-api'
+import { Button } from '@/components/ui/button'
 
 async function ManagerReportsPanel({ myManageesReports }) {
   const manageereport = myManageesReports.map((user) => user._id.targetId)
@@ -20,9 +21,22 @@ async function ManagerReportsPanel({ myManageesReports }) {
         <PanelHeader variant="highlight">
           <PanelTitle>Reports</PanelTitle>
         </PanelHeader>
-        <PanelContent className="p-10">
-          {users.map((user) => (
-            <PanelTitle>{user.fullName}</PanelTitle>
+        <PanelContent className="p-2 items-center">
+          {users.map((user: any) => (
+            <div className="border-2 flex">
+              <Image
+                className="rounded-full mx-2"
+                src={user.photo}
+                width={50}
+                height={50}
+                alt={'profile picture'}
+              />
+              <div>
+                <PanelTitle>{user.fullName.split(' ')[0]}'s Report</PanelTitle>
+                <p>{user.teamName}'s Team</p>
+              </div>
+              <Button>Write a review</Button>
+            </div>
           ))}
         </PanelContent>
       </Panel>
