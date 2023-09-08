6c0fe92d29bdb188e9463200fa1d926f8474671a
Craig Ostrin <craigostrin@gmail.com>
2023-09-08 11:02:39 +0200

    CallToAction will say "peer" if there's only 1

--- apps/client/app/dashboard/(user)/CallToAction.tsx
+++ apps/client/app/dashboard/(user)/CallToAction.tsx
@@ -4,7 +4,7 @@ type Action = 'review' | 'nominate'
 function CallToAction({ action, numberOfPeers }: CallToActionProps) {
   return (
     <ul className='list-disc pl-4'>
-      <li>You have been asked to <span className='font-extrabold'>{action} {numberOfPeers} peers</span></li>
+      <li>You have been asked to <span className='font-extrabold'>{action} {numberOfPeers} peer{numberOfPeers !== 1 ? 's' : ''}</span></li>
     </ul>
   )
 }
